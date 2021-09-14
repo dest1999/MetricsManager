fe8dea62828f68e1b9131320f834b7fc82159515
Oleg <serv_all@mail.ru>
2021-09-14 16:08:20 +0300

    Репозиторий агента изменен на использование Dappera

--- MetricsAgent/DAL/SQLiteCommonMetricsRepository.cs
+++ MetricsAgent/DAL/SQLiteCommonMetricsRepository.cs
@@ -1,4 +1,5 @@
-﻿using MetricsAgent.Models;
+﻿using Dapper;
+using MetricsAgent.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
@@ -34,97 +35,37 @@ namespace MetricsAgent.DAL
         protected string _dbTableName; //cpumetrics
         public SQLiteCommonMetricsRepository()
         {
-            //_dbTableName = dbTableName;
         }
         public void Create(BaseMetricValue item)
         {
             using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-
-            cmd.CommandText = $"INSERT INTO {_dbTableName}(value, time) VALUES(@value, @time)";
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time.ToString("s") );//перед записью привели время в вид YYYY-MM-DDTHH:MM:SS
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            {
+                connection.Execute($"INSERT INTO {_dbTableName}(value, time) VALUES(@value, @time)", item);
+            }
         }
 
         public void Delete(int id)
         {
             using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = $"DELETE FROM {_dbTableName} WHERE id=@id";
-
-            cmd.Parameters.AddWithValue("@id", id);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute($"DELETE FROM {_dbTableName} WHERE id=@{id}");
         }
 
         public IList<BaseMetricValue> GetAll()
         {
             using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-
-            cmd.CommandText = $"SELECT * FROM {_dbTableName}";
-
-            var returnList = new List<BaseMetricValue>();
-
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
-            {
-                // пока есть что читать -- читаем
-                while (reader.Read())
-                {
-                    returnList.Add(new BaseMetricValue
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = reader.GetDateTime(2)
-                    });
-                }
-            }
-
-            return returnList;
+            return connection.Query<BaseMetricValue>($"SELECT * FROM {_dbTableName}").ToList();
         }
 
         public BaseMetricValue GetById(int id)
         {
             using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = $"SELECT * FROM {_dbTableName} WHERE id=@id";
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
-            {
-                // если удалось что то прочитать
-                if (reader.Read())
-                {
-                    return new BaseMetricValue
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = reader.GetDateTime(2)
-                    };
-                }
-                else
-                {
-                    return null;
-                }
-            }
+            return connection.QuerySingle<BaseMetricValue>($"SELECT * FROM {_dbTableName} WHERE id=@{id}");
         }
 
         public void Update(BaseMetricValue item)
         {
             using var connection = new SQLiteConnection(ConnectionString);
-            using var cmd = new SQLiteCommand(connection);
-            // прописываем в команду SQL запрос на обновление данных
-            cmd.CommandText = $"UPDATE {_dbTableName} SET value = @value, time = @time WHERE id=@id;";
-            cmd.Parameters.AddWithValue("@id", item.Id);
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time );
-            cmd.Prepare();
-
-            cmd.ExecuteNonQuery();
+            connection.Execute($"UPDATE {_dbTableName} SET value = @value, time = @time WHERE id=@id;", item);
         }
     }
 }
